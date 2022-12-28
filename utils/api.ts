c856d8bab326736bf81befdd3012d5af4aed554e
李俊杰 <1091457391@qq.com>
2022-12-28 14:15:06 +0800

    feat: initial nuxt3

---
+++ utils/api.ts
@@ -0,0 +1,10 @@
+// import Http from '@/utils/request'
+import Http from '@/utils/http'
+ 
+// export const config1 = (params: any) => {
+//     return Http.get('https://www.baidu.com/', params)
+// }
+ 
+// export const config2 = (params: any) => {
+//     return Http.post('接口地址', params)
+// }

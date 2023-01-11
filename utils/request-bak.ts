2c99248f7054b5d34ca5b1c32cef76d771b66510
李俊杰 <1091457391@qq.com>
2023-01-11 21:34:08 +0800

    feat: request

--- utils/request-bak.ts
+++ utils/request-bak.ts
@@ -1,26 +1,42 @@
+import { _AsyncData } from 'nuxt3/dist/app/composables/asyncData'
 import { ElMessage } from 'element-plus'
+
+// 指定后端返回的基本数据类型
+export interface ResponseConfig {
+    code: number,
+    status: number,
+    data: any,
+    msg: string
+}
+export interface ValueConfig {
+    value: any,
+}
 
 const fetch = (url: string, options?: any): Promise<any>  => {
     const token = useCookie("token");
     const headers = { // headers信息
         'Authorization' : `Bearer ${token.value}`
     }
-    const { public: { baseUrl } } = useRuntimeConfig()
+    const { public: { baseUrl } } = useRuntimeConfig() // 3.0正式版环境变量要从useRuntimeConfig里的public拿
     const reqUrl = baseUrl + url
     return new Promise((resolve, reject) => {
-        useFetch(reqUrl, { ...options, headers }).then(({ data }: any) => {
+        useFetch(reqUrl, { ...options, headers }).then(({ data, error }: _AsyncData) => {
+            if (error.value) {
+                reject(error.value)
+                return
+            }
             const value = data.value
-            if (!data._rawValue) {
+            if (!value) {
                 // 这里处理错误回调
-                reject(value)
-            }else if(data._rawValue.code !== '0'){
+                // reject(value)
+                // $router.replace('/reject/' + value.status)
+            }else if(value.code == 102){
                 ElMessage({
-                    message: data._rawValue.msg,
+                    message: value.msg,
                     type: 'error',
                 })
             } else {
-                console.log('40data', data._rawValue)
-                resolve(ref(data))
+                resolve(ref(value))
             }
         }).catch((err: any) => {
             reject(err)
